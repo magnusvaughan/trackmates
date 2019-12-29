31717a88dd70f6e4416d75de9c7c71bc844f48a1
mvaughan <magnus.vaughan@gmail.com>
2019-12-29 12:28:55 +0000

    intitial commit

---
+++ resources/js/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        <div className="card-header">Welcome to Trackmates</div>
+
+                        <div className="card-body">Let's find you a collaborator</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
+
+if (document.getElementById('app')) {
+    ReactDOM.render(<App />, document.getElementById('app'));
+}
